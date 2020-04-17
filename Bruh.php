aaa2d885b70cc7a02f4b216f6d06331c9b785341
Francesco Roffia <francescoroffia@gmail.com>
2020-04-17 19:04:22 +0200

    Bruh php 3

--- Bruh.php
+++ Bruh.php
@@ -6,7 +6,24 @@
   </head>
   <body>
     <center>
-      <h1 style="color:red">Kevin non sa fare niente</h1>
+      <h1 id="kevin" style="color:red">Kevin non sa fare niente</h1>
     </center>
   </body>
+  <script>
+    var arr = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
+    var app = "";
+
+    function cambia(){
+      app = "#";
+
+      for(var i = 0;i < 6;i++){
+        app += arr[parseInt(Math.random()*16)];
+      }
+
+      document.getElementById("kevin").style.color = app;
+    }
+
+    setInterval(cambia,500);
+
+  </script>
 </html>
