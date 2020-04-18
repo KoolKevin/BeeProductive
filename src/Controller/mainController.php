90b200dede4069def18bb2575a1ba54158c42156
koolkevin <kevinkoltraka@gmail.com>
2020-04-18 19:40:21 +0200

    tema e roba

---
+++ src/Controller/mainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+    class mainController extends AbstractController{   
+        /**
+        * @Route("/home")
+        */
+        
+        public function home() {
+            return $this->render('index.html.twig');
+        }
+    }
